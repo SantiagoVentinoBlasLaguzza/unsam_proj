31d3b599578006be5bb650ef3ac016f5b54a1720
Santiago Valentino Blas Laguzza <sblas@imal.unl.edu.ar>
2025-02-13 15:07:41 -0300

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 def zero_diagonals(tensor: torch.Tensor) -> torch.Tensor:
     if tensor.dim() == 2:
@@ -9,3 +10,15 @@ def zero_diagonals(tensor: torch.Tensor) -> torch.Tensor:
             diag_indices = torch.eye(tensor.shape[1], device=tensor.device).bool()
             tensor[c][diag_indices] = 0
     return tensor
+
+def set_random_seed(seed: int) -> None:
+    """
+    Set the seed for reproducibility in numpy and torch.
+    
+    Args:
+        seed (int): Seed value.
+    """
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    logging.info(f"Random seed set to: {seed}")
+
